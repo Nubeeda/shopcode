4ac6f5747b698b233c08c37e2a44b9894b4e42de
nubeeda <mehakbaloch14122001@gmail.com>
2025-02-17 06:07:58 -0800

    form

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -8,7 +8,11 @@ const Profile = () => {
             <i className="ri-user-line text-4xl"></i>
             <h1 className="text-4xl font-semibold">Profile </h1>
           </div>
-          <hr className="text-gray-300 my-6" />
+          <hr className="text-gray-300 my-6 " />
+          <div className="mx-auto w-24 h-24 relative mb-6">
+            <img src="/images/ava.png" className="w-24 h-24"/>
+            <input type="file" accept="image/*" className="h-full w-full absolute left-0 top-0 opacity-0"/>
+          </div>
           <form className="grid grid-cols-2">
             <div className="flex flex-col">
             <label className="font-semibold mb-2">FullName</label>
@@ -35,6 +39,56 @@ const Profile = () => {
             className="border border-gray-300 w-96 rounded p-2"
              />
              </div>
+             <div/>
+             <div className="flex flex-col my-3 col-span-2">
+            <label className="font-semibold mb-2">Area/street/vill</label>
+            <input 
+            name="Address"
+            type="text"
+            value="Street 12 madina town khanpur"
+            className="border border-gray-300 w-96 rounded p-2"
+             />
+             </div>
+             <div className="flex flex-col my-3">
+            <label className="font-semibold mb-2">City</label>
+            <input 
+            name="city"
+            type="text"
+            value="khanpur"
+            className="border border-gray-300 w-96 rounded p-2"
+             />
+             </div>
+             <div className="flex flex-col my-3">
+            <label className="font-semibold mb-2">State</label>
+            <input 
+            name="state"
+            type="text"
+            value="Punjab"
+            className="border border-gray-300 w-96 rounded p-2"
+             />
+             </div>
+             <div className="flex flex-col my-3">
+            <label className="font-semibold mb-2">Country</label>
+            <input 
+            name="country"
+            type="text"
+            value="Pakistan"
+            className="border border-gray-300 w-96 rounded p-2"
+             />
+             </div>
+             <div className="flex flex-col my-3">
+            <label className="font-semibold mb-2">PostalCode</label>
+            <input 
+            name="postalcode"
+            type="number"
+            value="64100"
+            className="border border-gray-300 w-96 rounded p-2"
+             />
+             </div>
+             <button className="px-4 py-2 rounded bg-rose-600 text-white w-fit hover:bg-green-600">
+             <i className="ri-save-line mr-2"></i>
+             Save
+             </button>
           </form>
         </div>
       </div>
